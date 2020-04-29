dc9fff5af593c151804768d97193fb27a1a61dae
Saiteju1997 <51901826+Saiteju1997@users.noreply.github.com>
2020-04-29 12:33:48 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-ls /inet/workspace/jfrog-example/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war | tail -1 | xargs cp -t /inet/workspace/jfrog-example/
-\rm -rf /inet/workspace/jfrog-example/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war
+ls /inet/workspace/Capstone1-project/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war | tail -1 | xargs cp -t /inet/workspace/Capstone1-project/
+\rm -rf /inet/workspace/Capstone1-project/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*
