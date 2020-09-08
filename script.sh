62c8f2827e8d3bd84c0ff9862a0c8bc9d64f9099
Harthika <60595711+Harthika@users.noreply.github.com>
2020-09-08 16:32:30 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,3 @@
-ls /inet/workspace/Capstone1-project/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war | tail -1 | xargs cp -t /inet/workspace/Capstone1-project/
-\rm -rf /inet/workspace/Capstone1-project/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*
+ls /DevOps/workspace/samplejob/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*.war | tail -1 | xargs cp -t /DevOps/
+mv  /DevOps/*.war /DevOps/ansible/ILP_Bookstore.war
+\rm -rf /DevOps/workspace/samplejob/Bazinga/devops/ilp1/ILP/0.0.1-SNAPSHOT/*
